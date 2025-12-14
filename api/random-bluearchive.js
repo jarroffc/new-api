bc9540622d2f37513967a4015d3c1d9273c8db7a
Junzz138 <eppestok92@gmail.com>
2025-12-14 08:06:58 +0700

    Add files via upload

---
+++ api/random-bluearchive.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: "Blue Archive",
+  desc: "Random Blue Archive",
+  category: "Random",
+  path: "/random/ba?apikey=",
+  async run(req, res) {
+    const { apikey } = req.query;
+    if (!apikey || !global.apikey.includes(apikey)) {
+      return res.json({ status: false, error: 'Apikey invalid' });
+    }
+
+    try {
+      const data = await fetchJson(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`);
+      const image = await getBuffer(data.url);
+
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': pedo.length
+      });
+      res.end(pedo);
+    } catch (error) {
+      res.status(500).json({ status: false, error: error.message });
+    }
+  }
+};
